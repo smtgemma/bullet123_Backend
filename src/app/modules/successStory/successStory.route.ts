e5fad3839d0ba9b3959415d006797efe636dc22a
md-sanim-mia <hasansanim562@gmail.com>
2026-04-30 21:50:40 +0600

    add permotion

--- src/app/modules/successStory/successStory.route.ts
+++ src/app/modules/successStory/successStory.route.ts
@@ -8,7 +8,7 @@ const router = Router();
 // Create
 router.post(
   "/add",
-  auth(UserRole.REALTOR, UserRole.ADMIN, UserRole.SUPER_ADMIN), // Restricted to Realtors and Admins
+  auth(UserRole.REALTOR, UserRole.ADMIN, UserRole.SUPER_ADMIN,UserRole.COMMUNITY_PARTNER,UserRole.MUNICIPALITY), // Restricted to Realtors and Admins
   SuccessStoryController.createSuccessStory
 );
 
@@ -16,7 +16,7 @@ router.post(
 router.get("/all", SuccessStoryController.getAllSuccessStories);
 
 // Get my own success stories
-router.get("/my-stories", auth(UserRole.REALTOR, UserRole.ADMIN, UserRole.SUPER_ADMIN), SuccessStoryController.getMySuccessStories);
+router.get("/my-stories", auth(UserRole.REALTOR, UserRole.ADMIN, UserRole.SUPER_ADMIN,UserRole.COMMUNITY_PARTNER,UserRole.MUNICIPALITY), SuccessStoryController.getMySuccessStories);
 
 // Get success stories of a specific professional
 router.get("/professional/:professionalId", SuccessStoryController.getSuccessStoriesByProfessional);
